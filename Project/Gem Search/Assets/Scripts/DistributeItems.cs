749a067f8f26cb1fb8b3a43ef010df0fe5a3d04d
KieranCoughlan <KieranCoughlan@gmail.com>
2023-01-01 14:49:48 +0000

    Working item distributor

--- Project/Gem Search/Assets/Scripts/DistributeItems.cs
+++ Project/Gem Search/Assets/Scripts/DistributeItems.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class DistributeItems : MonoBehaviour
 {
-    public float borderWidth;
-    public float spacing;
-    public float percentage;
+    public float spacing = 1.0f;
+    public float percentage = 1.0f;
     public string layerName;
-    public GameObject prefab;
+    public GameObject[] prefabs;
     public Vector3 size;
 
     // Start is called before the first frame update
@@ -25,16 +24,33 @@ public class DistributeItems : MonoBehaviour
 
     private void Distribute()
     {
-        // Find mesh extents
-        MeshRenderer mr = GetComponent<MeshRenderer>();
-        Debug.Log(transform.position);
-        Debug.Log(mr.bounds.center);
-        Debug.Log(mr.bounds.extents);
-
         LayerMask lm = LayerMask.NameToLayer(layerName);
 
-        //for (float )
-        //Physics.Raycast()
+        // Start at position - half size in x and z and plus half size in y
+        Vector3 startPosition = transform.position +
+                                new Vector3(-size.x/2.0f, size.y/2.0f, -size.z/2.0f);
+
+        for (float xPos = 0; xPos <= size.x; xPos += spacing )
+        {
+            for (float zPos = 0; zPos <= size.z; zPos += spacing)
+            {
+                Vector3 thisPos = startPosition + new Vector3(xPos, 0, zPos);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(thisPos, Vector3.down, out hitInfo,
+                                    size.y, lm))
+                {
+                  if (Random.value < percentage)
+                  {
+                        // Spawn a prefab
+                        int prefabChoice = Random.Range(0, prefabs.Length);
+                        Instantiate(prefabs[prefabChoice], hitInfo.point,
+                                    prefabs[prefabChoice].transform.rotation,
+                                    transform);
+                  }
+                }
+            }
+        }
+
     }
 
     void OnDrawGizmos()
