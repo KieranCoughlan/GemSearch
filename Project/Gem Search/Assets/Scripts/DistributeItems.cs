fb4ff308138f12aa9dd468bf897724fe299410a2
KieranCoughlan <KieranCoughlan@gmail.com>
2023-01-05 17:29:20 +0000

    Testing "nearest"

--- Project/Gem Search/Assets/Scripts/DistributeItems.cs
+++ Project/Gem Search/Assets/Scripts/DistributeItems.cs
@@ -4,60 +4,106 @@ using UnityEngine;
 
 public class DistributeItems : MonoBehaviour
 {
-    public float spacing = 1.0f;
-    public float percentage = 1.0f;
-    public string layerName;
-    public GameObject[] prefabs;
-    public Vector3 size;
+  public float spacing = 1.0f;
+  public float percentage = 1.0f;
+  public string layerName;
+  public GameObject[] prefabs;
+  public GameObject[] generatedItems;
+  public Vector3 size;
+  public GameObject Player;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    Distribute();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    TrackPlayer();
+  }
+
+  private void Distribute()
+  {
+    LayerMask lm = LayerMask.NameToLayer(layerName);
+
+    // Start at position - half size in x and z and plus half size in y
+    Vector3 startPosition = transform.position +
+                            new Vector3(-size.x / 2.0f, size.y / 2.0f, -size.z / 2.0f);
+
+    List<GameObject> genItems = new List<GameObject>();
+
+    for (float xPos = 0; xPos <= size.x; xPos += spacing)
     {
-        Distribute();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void Distribute()
-    {
-        LayerMask lm = LayerMask.NameToLayer(layerName);
-
-        // Start at position - half size in x and z and plus half size in y
-        Vector3 startPosition = transform.position +
-                                new Vector3(-size.x/2.0f, size.y/2.0f, -size.z/2.0f);
-
-        for (float xPos = 0; xPos <= size.x; xPos += spacing )
+      for (float zPos = 0; zPos <= size.z; zPos += spacing)
+      {
+        Vector3 thisPos = startPosition + new Vector3(xPos, 0, zPos);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(thisPos, Vector3.down, out hitInfo,
+                            size.y, lm))
         {
-            for (float zPos = 0; zPos <= size.z; zPos += spacing)
-            {
-                Vector3 thisPos = startPosition + new Vector3(xPos, 0, zPos);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(thisPos, Vector3.down, out hitInfo,
-                                    size.y, lm))
-                {
-                  if (Random.value < percentage)
-                  {
-                        // Spawn a prefab
-                        int prefabChoice = Random.Range(0, prefabs.Length);
-                        Instantiate(prefabs[prefabChoice], hitInfo.point,
-                                    prefabs[prefabChoice].transform.rotation,
-                                    transform);
-                  }
-                }
-            }
+          if (Random.value < percentage)
+          {
+            // Spawn a prefab
+            int prefabChoice = Random.Range(0, prefabs.Length);
+            GameObject go = Instantiate(prefabs[prefabChoice], hitInfo.point,
+                                        prefabs[prefabChoice].transform.rotation,
+                                        transform);
+            genItems.Add(go);
+          }
         }
-
+      }
     }
 
-    void OnDrawGizmos()
+    generatedItems = genItems.ToArray();
+  }
+
+  void OnDrawGizmos()
+  {
+    // Draw a yellow sphere at the transform's position
+    Gizmos.color = Color.yellow;
+
+    Gizmos.DrawWireCube(transform.position, size);
+  }
+
+  public GameObject NearestItem(Vector3 to)
+  {
+    GameObject nearest = generatedItems[0];
+    float dist = (nearest.transform.position - to).magnitude;
+
+    for (int i = 1; i < generatedItems.Length; i++)
     {
-        // Draw a yellow sphere at the transform's position
-        Gizmos.color = Color.yellow;
+      GameObject thisItem = generatedItems[i];
+      float thisDist = (thisItem.transform.position - to).magnitude;
 
-        Gizmos.DrawWireCube(transform.position, size);
+      if (thisDist < dist)
+      {
+        nearest = thisItem;
+        dist = thisDist;
+      }
     }
+
+    return nearest;
+  }
+
+  public void TrackPlayer()
+  {
+    if (Player == null)
+      return;
+
+    GameObject nearest = NearestItem(Player.transform.position);
+
+    for (int i = 0; i < generatedItems.Length; i++)
+    {
+      SetIndicator(generatedItems[i], generatedItems[i] == nearest);
+    }
+  }
+
+  private void SetIndicator(GameObject go, bool isNearest)
+  {
+    Transform indicator = go.transform.Find("ClosestIndicator");
+    if (indicator != null)
+      indicator.gameObject.SetActive(isNearest);
+  }
 }
